ba0411decf7da76246e1dceb8ec6322203ee1f4e
wisdom6715 <olayiwolaibrahim46@gmail.com>
2024-11-02 18:30:03 +0100

    on exercises

--- Data Structure/Array/reverseString.js
+++ Data Structure/Array/reverseString.js
@@ -1,6 +1,11 @@
 function reverse(str){
+  /// you can replace the spread operator with split method in javascript
   const reversedstring = [...str].reverse().join('');
   console.log(reversedstring)
 }
-
+/// we can still make the code better using ECS 6
+const reverse = (str) => {
+  const reversedString = [...str].reverse().join('');
+  console.log(reversedString)
+}
 reverse('man')
